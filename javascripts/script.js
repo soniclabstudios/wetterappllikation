0c86c7b1ead25b30a3fa43156cf37646a5b4a50e
Florian Gächter <florian.gaechter@gmail.com>
2014-05-23 10:42:04 +0200

    Add forecast.io service call

--- javascripts/script.js
+++ javascripts/script.js
@@ -5,11 +5,18 @@ var options = {
 };
 
 function success(pos) {
-  var crd = pos.coords;
+	var crd = pos.coords;
 
-  $('.js-lat').text(crd.latitude);
-  $('.js-long').text(crd.longitude);
-  $('.js-acc').text(crd.accuracy + ' m');
+	$('.js-lat').text(crd.latitude);
+	$('.js-long').text(crd.longitude);
+	$('.js-acc').text(crd.accuracy + ' m');
+
+	$.ajax({
+		url: 'https://api.forecast.io/forecast/a955df0e9afe8c822ebb3adf30265fb6/37.8267,-122.423',
+		success: function(data) {
+		console.log(data);
+		}
+	});
 }
 
 function error(err) {
